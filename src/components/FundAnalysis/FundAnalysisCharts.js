3963f5dd7d0cb4f010cfebdaf42b174cfc4138c6
151250121 <151250121@smail.nju.edu.cn>
2017-09-14 21:15:21 +0800

    <fix>:<add % to the data>

--- src/components/FundAnalysis/FundAnalysisCharts.js
+++ src/components/FundAnalysis/FundAnalysisCharts.js
@@ -95,8 +95,8 @@ function FundAnalysisCharts({
       <div className="card" id="4">
         <DivHeader>业绩持续性指标</DivHeader>
         <p className={styles.description}>持续性指标为{fundPerformanceIndex ? fundPerformanceIndex.sustainabilityIndex : null}，
-          连输期数／总期数为{fundPerformanceIndex ? fundPerformanceIndex.loseDayRatio : null}，
-          连赢期数／总期数为{fundPerformanceIndex ? fundPerformanceIndex.winDayRatio : null}</p>
+          连输期数／总期数为{fundPerformanceIndex ? fundPerformanceIndex.loseDayRatio + '%' : null}，
+          连赢期数／总期数为{fundPerformanceIndex ? fundPerformanceIndex.winDayRatio + '%' : null}</p>
       </div>
 
       {fundStyleAttributionProfit && fundStyleAttributionRisk ?
